69ab1d1800ef8ffdfd1e87f2d10678cdc7433717
akanik0 <nniko6740@gmail.com>
2023-05-11 08:44:34 +0400

    solved problems during lesson

--- src/az/coders/chapter1/lesson5/PrimeNumbers.java
+++ src/az/coders/chapter1/lesson5/PrimeNumbers.java
@@ -3,9 +3,17 @@ package az.coders.chapter1.lesson5;
 public class PrimeNumbers {
 
     public static void main(String[] args) {
-        int num = 100;
-        for (int i = 0; i < Math.sqrt(num); i++) {
-            
+        int beginIndex = 1;
+        int endingIndex = 100;
+
+        OUTER:
+        for (int i = beginIndex; i < endingIndex; i++) {
+            for (int j = i-1; j >= 2 ; j--) {
+                if( i % j == 0){
+                    continue OUTER;
+                }
+            }
+            System.out.println(i);
         }
     }
 }
