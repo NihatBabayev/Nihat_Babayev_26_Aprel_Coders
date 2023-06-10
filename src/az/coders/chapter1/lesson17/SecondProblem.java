a84443620094da540d9ed4aeacf9992d44be1d54
akanik0 <nniko6740@gmail.com>
2023-06-10 11:42:39 +0400

    push 3*10^7

--- src/az/coders/chapter1/lesson17/SecondProblem.java
+++ src/az/coders/chapter1/lesson17/SecondProblem.java
@@ -1,20 +1,27 @@
 package az.coders.chapter1.lesson17;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Stream;
 
 public class SecondProblem {
     public static void main(String[] args) {
-        List<String> stringList = new ArrayList<>();
-        stringList.add("Nihat");
-        stringList.add("NNNNN");
-        stringList.add("MMMM");
-        stringList.add("inin");
-        stringList.add("abcdef");
-        stringList.add("abc");
+        List<String> words = new ArrayList<>();
+        words.add("Nihat");
+        words.add("NNNNN");
+        words.add("MMMM");
+        words.add("inin");
+        words.add("abcdef");
+        words.add("abc");
+        List<String> result = words.stream()
+                .filter(word -> word.chars().distinct().count() >= 3)
+                .sorted(Comparator.comparingInt(String::length).reversed())
+                .limit(5)
+                .map(String::toUpperCase)
+                .toList();
 
-//        List<String> result = stringList.stream().filter(n -> )
+        System.out.println(result);
 
 
     }
